aa14fd9fdefd83df93513b4cbb0bd279b103e6dc
Md Abdul A ripon <ripon@Mds-MacBook-Pro.local>
2017-07-22 20:46:14 -0700

    add bootstrap and css with some reactive form concept

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, FormArray } from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+    title = 'REACTIVE FORM || MODEL DRIVEN FORM';
+
+    userForm = new FormGroup({
+        name: new FormControl(),
+        email: new FormControl(),
+        address: new FormGroup({
+            street: new FormControl(),
+            city: new FormControl(),
+            state: new FormControl(),
+            // zipcode: new FormArray({
+            //     // zipcode: new FormControl(),
+            // })
+        })
+    });
 }
